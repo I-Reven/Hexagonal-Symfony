178d842c16452b1c96d356659f249c57df40d97c
kousha <kousha.ghodsizad@gmail.com>
2020-05-08 21:20:43 +0300

    install phpunit

--- src/config/bundles.php
+++ src/config/bundles.php
@@ -2,4 +2,5 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Liip\FunctionalTestBundle\LiipFunctionalTestBundle::class => ['dev' => true, 'test' => true],
 ];
